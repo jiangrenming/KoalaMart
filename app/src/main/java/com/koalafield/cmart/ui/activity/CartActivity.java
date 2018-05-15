887a874ff6864c21380bef4a77d0a88cb338c2bf
jiangrenming <1522318682@qq.com>
2018-05-16 00:24:50 +0800

    购物车接口

--- app/src/main/java/com/koalafield/cmart/ui/activity/CartActivity.java
+++ app/src/main/java/com/koalafield/cmart/ui/activity/CartActivity.java
@@ -4,6 +4,9 @@ import android.content.Intent;
 
 import com.koalafield.cmart.R;
 import com.koalafield.cmart.base.activity.BaseActivity;
+import com.koalafield.cmart.bean.cart.CartDataBean;
+import com.koalafield.cmart.presenter.cart.CartListPresenter;
+import com.koalafield.cmart.ui.view.cart.ICartListView;
 import com.koalafield.cmart.utils.ShareBankPreferenceUtils;
 import com.koalafield.cmart.utils.StringUtils;
 
@@ -13,7 +16,7 @@ import com.koalafield.cmart.utils.StringUtils;
  * @date 2018/5/13
  */
 
-public class CartActivity extends TabBaseActivity {
+public class CartActivity extends TabBaseActivity implements ICartListView<CartDataBean>{
 
     @Override
     public int attchLayoutRes() {
@@ -29,11 +32,27 @@ public class CartActivity extends TabBaseActivity {
 
     @Override
     public void initDatas() {
-
+        presenter = new CartListPresenter(CartActivity.this);
+        presenter.getData();
     }
 
     @Override
     public void upDateViews() {
+
+    }
+
+    @Override
+    public void onSucessCartFul(CartDataBean data) {
+
+    }
+
+    @Override
+    public void onFailureCart(String message) {
+
+    }
+
+    @Override
+    public void onLoadNoData() {
 
     }
 }
